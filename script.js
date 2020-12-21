6317b1145454edc3ac8a86aac65f10b4af3697a9
blaucomfuig <sergio.corcuera01@estudiant.upf.edu>
2020-12-21 11:13:07 +0100

    primera actualizacion

---
+++ script.js
@@ -0,0 +1,50 @@
+const btnHappy = document.getElementById("happy")
+
+const btnSad = document.getElementById("sad")
+
+
+const SAD = document.querySelector(".sad")
+const HAPPY = document.querySelector(".happy")
+
+
+
+btnHappy.addEventListener('click', () => {
+   SAD.src = "happymarguerite.jpg"
+})
+
+btnSad.addEventListener('click', () => {
+    HAPPY.src = "sadmarguerite.jpg"
+})
+
+function bonicaCanço() {
+    let cançodenit = document.getElementById("canço")
+    cançodenit.src="bonica.mp3"
+    cançodenit.play()
+}
+
+function noCanço(){
+    let cançodenit = document.getElementById("canço")
+    cançodenit.src="bonica.mp3"
+    cançodenit.pause()
+}
+
+
+ScrollReveal().reveal('.pregunta', {
+    delay: 500,
+    duration: 1500,
+    origin: "top",
+    distance: "200px"
+})
+
+ScrollReveal().reveal('.btn', {
+    delay: 1000,
+    duration: 2500,
+
+})
+
+ScrollReveal().reveal('.sad', {
+    delay: 2000,
+    duration: 2500,
+
+})
+
